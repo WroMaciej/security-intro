95c0955e81f8955a3eee75b3ccd8cf975aa75cf5
WroMaciej <maciej.a.wroblewski@capgemini.com>
2019-01-14 13:27:27 +0100

    Init

---
+++ src/main/java/com/wromaciej/securityintro/SecurityIntroApplication.java
@@ -0,0 +1,14 @@
+package com.wromaciej.securityintro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityIntroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityIntroApplication.class, args);
+	}
+
+}
+
