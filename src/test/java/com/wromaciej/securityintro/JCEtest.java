f9fcf01924a2267ca66b8a61c501a70e2bc01fb9
WroMaciej <maciej.a.wroblewski@capgemini.com>
2019-03-13 10:18:52 +0100

    Update test for key length

--- src/test/java/com/wromaciej/securityintro/JCEtest.java
+++ src/test/java/com/wromaciej/securityintro/JCEtest.java
@@ -1,16 +1,23 @@
 package com.wromaciej.securityintro;
 
+import static org.junit.Assert.assertThat;
+
 import java.security.NoSuchAlgorithmException;
 
+import static org.hamcrest.Matchers.*;
 import org.junit.Test;
 
 public class JCEtest {
 	
 	@Test
 	public void shouldHaveUnlimitedKey() throws NoSuchAlgorithmException {
-		
+		//given data in JDK
+		//when
 		int maxKeySize = javax.crypto.Cipher.getMaxAllowedKeyLength("AES");
-		System.out.println("Max Key Size for AES : " + maxKeySize);
+		//then
+		assertThat(maxKeySize, greaterThan(1024) );		
 	}
+	
+	
 
 }
